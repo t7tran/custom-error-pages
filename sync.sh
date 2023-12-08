33aba8336fc95a011fcc61ecc5efe4420f779f78
Tien Tran <tien@transfamily.info>
2023-12-08 12:01:38 +1100

    Sync with ingress-nginx-controller-v1.9.4

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-version=1.8.0
+version=1.9.4
 
 curl -L https://github.com/kubernetes/ingress-nginx/archive/refs/tags/controller-v${version}.zip -o /tmp/controller.zip
 mkdir /tmp/controller
 unzip /tmp/controller.zip -d /tmp/controller
+rm -rf ./etc/* ./www/*
 mv /tmp/controller/ingress-nginx-controller-v${version}/images/custom-error-pages/rootfs/* .
